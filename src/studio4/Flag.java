4c1bd4410da390176b711bbacd257bda72165b6d
tylerleiderman <tylerleiderman@localhost>
2024-10-02 14:15:49 -0500

    Studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,20 @@
 package studio4;
 
 import java.awt.Color;
+import java.io.File;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.filledRectangle(.5, .5, .2, .2);
+		StdDraw.setPenColor(StdDraw.MAGENTA);
+			StdDraw.filledCircle( .5, .5, .1);
+		
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,23 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shape = in.next();
+		int red = in.nextInt();
+		int green= in.nextInt();
+		int blue= in.nextInt();
+		boolean filled = in.nextBoolean(); 
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfwidth = in.nextDouble();
+		double halflength = in.nextDouble();
+		StdDraw.setPenColor(red, green, blue);
+		if (filled == true) {
+			StdDraw.filledRectangle(x, y, halfwidth, halflength);
+		}
+		else {
+			StdDraw.rectangle(x, y, halfwidth, halflength);
+
+		}
 		
 	}
 }
